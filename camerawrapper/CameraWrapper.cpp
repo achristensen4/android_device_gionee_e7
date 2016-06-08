7c4e9c1ce5d0f132e3d9ffe7af3ac3a7b009a299
feroxx <frhatfrat@gmail.com>
2016-06-08 19:34:32 +0300

    Update CameraWrapper.cpp

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -109,10 +109,8 @@ static char *camera_fixup_getparams(int __attribute__((unused)) id,
     params.dump();
 #endif
 
-    params.remove("selected-auto-scene");
-    params.remove("scene-detect-values");
-    params.remove("food");
-    params.remove("gesture");
+    params.set(android::CameraParameters::SCENE_MODE_GESTURE, "gesture");
+    params.set(android::CameraParameters::SCENE_MODE_FOOD, "food");
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
@@ -134,6 +132,9 @@ static char *camera_fixup_setparams(int id, const char *settings)
     ALOGV("%s: original parameters:", __FUNCTION__);
     params.dump();
 #endif
+
+    params.set(android::CameraParameters::SCENE_MODE_GESTURE, "gesture");
+    params.set(android::CameraParameters::SCENE_MODE_FOOD, "food");
 
     android::String8 strParams = params.flatten();
 
