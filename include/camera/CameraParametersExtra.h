21c3bc2e1ca5f8529c3d0ecb44b582cedbe29e24
feroxx <frhatfrat@gmail.com>
2016-06-10 13:19:43 +0300

    Update CameraParametersExtra.h

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -1,11 +1,11 @@
 #define CAMERA_PARAMETERS_EXTRA_C  \
 const char CameraParameters::SCENE_MODE_GESTURE[] = "gesture"; \
 const char CameraParameters::SCENE_MODE_FOOD[] = "food"; \
-void CameraParameters::setAEBracket(const char *mode) { set(EXP_BRACKETING_OFF, mode); }; \
+int CameraParameters::setAEBracket(const char *mode) { set(EXP_BRACKETING_OFF, mode); }; \
 int CameraParameters::getStr(const char *key) const { return -1; };
 
 #define CAMERA_PARAMETERS_EXTRA_H \
 static const char SCENE_MODE_GESTURE[]; \
 static const char SCENE_MODE_FOOD[]; \
-void setAEBracket(const char *mode); \
+int setAEBracket(const char *mode); \
 int getStr(const char *key) const;
