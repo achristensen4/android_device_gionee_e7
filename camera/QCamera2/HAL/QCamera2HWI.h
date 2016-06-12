96ad27db92bd0c18d51451ab94f6338ff603334b
feroxx <frhatfrat@gmail.com>
2016-06-12 22:36:34 +0300

    Update QCamera2HWI.h

--- camera/QCamera2/HAL/QCamera2HWI.h
+++ camera/QCamera2/HAL/QCamera2HWI.h
@@ -282,8 +282,10 @@ private:
     bool needRotationReprocess();
     void debugShowVideoFPS();
     void debugShowPreviewFPS();
+    void dumpJpegToFile(const void *data, uint32_t size, int index);
     void dumpFrameToFile(QCameraStream *stream,
-            mm_camera_buf_def_t *frame, uint32_t dump_type);
+                         mm_camera_buf_def_t *frame,
+                         int dump_type);
     void releaseSuperBuf(mm_camera_super_buf_t *super_buf);
     void playShutter();
     void getThumbnailSize(cam_dimension_t &dim);
