d71ab99cee04e820cdc0a22df5c121a0d0c22e4f
feroxx <frhatfrat@gmail.com>
2016-06-28 01:12:27 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -128,7 +128,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8974 \
     gralloc.msm8974 \
     copybit.msm8974 \
-    camera.msm8974 \
     memtrack.msm8974 \
     lights.msm8974 \
     keystore.msm8974 \
