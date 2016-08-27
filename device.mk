7422fda53b9d45851a2fde66eba9e233b07b40c2
feroxx <frhatfrat@gmail.com>
2016-08-27 12:25:37 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -104,7 +104,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.bcm2079x.default \
     NfcNci \
-    Tag
+    Tag \
+    SnapdragonCamera
  
  PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
